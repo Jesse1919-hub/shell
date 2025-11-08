36feb73002b780e4a034ea50ef5938e553f62393
Jesse Segura <jesse_segura@yahoo.com>
2025-11-08 09:31:57 -0500

    Q1 and Q2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -26,11 +26,11 @@ unzip -q rawdata.zip
 
 ###########################################
 # Complete assignment here
-mkdir Data
+
 # 1. Create a directory named data
-
+mkdir Data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv rawdata data/raw
 # 3. List the contents of the ./data/raw directory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
